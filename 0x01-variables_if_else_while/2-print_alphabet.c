bb2d7db6920ff8e4ac4afeb571881b29885b2458
zamo <editingmom1000@gmail.com>
2023-02-20 16:24:38 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
+
