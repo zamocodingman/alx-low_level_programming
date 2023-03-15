7aa85d1757a88334883ce829075c4cf994dc641f
zamo <editingmom1000@gmail.com>
2023-03-15 14:23:01 -0700

    push

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,4 @@
+#ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
 int _islower(int c);
@@ -19,5 +20,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
 
