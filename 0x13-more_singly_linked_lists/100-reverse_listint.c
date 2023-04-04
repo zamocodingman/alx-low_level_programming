c1d8e4c094b3c4b3966f051c2ff9811b98a41b57
zamo <editingmom1000@gmail.com>
2023-04-04 16:26:28 -0700

    list3

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,92 +1,27 @@
 #include "lists.h"
-#include <stdio.h>
-
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
 
 /**
-* looped_listint_len - Counts the number of unique nodes
-* in a looped listint_t linked list.
-* @head: A pointer to the head of the listint_t to check.
+* reverse_listint - reverses a linked list
+* @head: pointer to the first node in the list
 *
-* Return: If the list is not looped - 0.
-* Otherwise - the number of unique nodes in the list.
+* Return: pointer to the first node in the new list
 */
 
-size_t looped_listint_len(const listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-const listint_t *tortoise, *hare;
-size_t nodes = 1;
-
-if (head == NULL || head->next == NULL)
-return (0);
-
-tortoise = head->next;
-hare = (head->next)->next;
-
-while (hare)
+listint_t *prev = NULL;
+listint_t *next = NULL;
+ 
+while (*head)
 {
-if (tortoise == hare)
-{
-tortoise = head;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-hare = hare->next;
+next = (*head)->next;
+(*head)->next = prev;
+prev = *head;
+*head = next;
 }
 
-tortoise = tortoise->next;
-while (tortoise != hare)
-} 
-nodes++; 
-tortoise = tortoise->next;
+*head = prev;
+
+return (*head);
 }
-
-return (nodes);
-}
-
-tortoise = tortoise->next;
-hare = (hare->next)->next;
-}
-
-return (0);
-}
-
-/**
-* print_listint_safe - Prints a listint_t list safely.
-* @head: A pointer to the head of the listint_t list.
-*
-* Return: The number of nodes in the list.
-*/
-
-size_t print_listint_safe(const listint_t *head)
-{
-size_t nodes, index = 0;
-
-nodes = looped_listint_len(head);
-
-if (nodes == 0)
-{
-for (; head != NULL; nodes++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-}
-
-else
-{
-for (index = 0; index < nodes; index++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-
-printf("-> [%p] %d\n", (void *)head, head->n);
-}
-
-return (nodes);
-}
-
 
